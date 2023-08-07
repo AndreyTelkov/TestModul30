d07661e809b17f8956a754a02a27fb74b515ed79
Andre Telkov <Andrey.TELKOV@wedomain.ru>
2023-08-07 20:46:01 +0300

    OK

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,19 @@
+public class PersonTest {
+    public static boolean isTeenager(int age)
+    {
+        boolean result;
+        if (age < 19) {
+            result = true;
+        }
+        else {
+            result = false;
+        }
+        return result;
+    }
+    System.out.println(result);
+}
+@test1
+ void testAge(){
+    int age = 20;
+    assertFalse(result);
+}
